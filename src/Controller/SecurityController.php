3835ced65ccc2510e19ca293c58a53acd4e4e137
Elhadj BAH <ismailabah16@gmail.com>
2021-11-13 18:52:38 +0100

    check user mis à jour pour la gestion d'authentification

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 
+use App\Entity\Utilisateurs;
 use App\Repository\UtilisateursRepository;
 use App\Services\Mailer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
--- src/Security/UserChecker.php
+++ src/Security/UserChecker.php
@@ -2,7 +2,27 @@
 
 namespace App\Security;
 
-class UserChecker
-{
+use App\Entity\Utilisateurs;
+use Symfony\Component\Security\Core\Exception\AccountStatusException;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
+use Symfony\Component\Security\Core\User\UserCheckerInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
+class UserChecker implements UserCheckerInterface
+{
+   public function checkPreAuth(UserInterface $user):void
+   {
+       if(!$user instanceof Utilisateurs){
+           return;
+       }
+
+       if($user->isVerified()==0){
+           throw new CustomUserMessageAuthenticationException('Vous devez cliquer sur le lien reçu par email lors de votre inscription pour activer votre compte pour pouvoir se connecter');
+       }
+   }
+
+    public function checkPostAuth(UserInterface $user)
+    {
+        // TODO: Implement checkPostAuth() method.
+    }
 }
